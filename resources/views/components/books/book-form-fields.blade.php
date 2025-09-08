fc646e863c1bcc3a6eb5b94c5a0cb7a6265f820a
HiCharly <caillon.charly@gmail.com>
2025-09-08 18:20:02 +0200

    feat: display book cover uploading loader

--- resources/views/components/books/book-form-fields.blade.php
+++ resources/views/components/books/book-form-fields.blade.php
@@ -8,12 +8,21 @@
             @if($form->thumbnail_url)
                 <img src="{{ $form->thumbnail_url }}" class="size-full object-cover" />
             @else
-                <div class="relative size-full bg-neutral-100 rounded-lg flex items-center justify-center cursor-pointer text-neutral-400 hover:text-neutral-600 hover:bg-neutral-200" x-on:click="$refs.coverInput.click()">
+                <div
+                    class="relative size-full bg-neutral-100 rounded-lg flex items-center justify-center cursor-pointer text-neutral-400 hover:text-neutral-600 hover:bg-neutral-200"
+                    x-on:click="$refs.coverInput.click()"
+                    x-data="{ uploading: false }"
+                    x-on:livewire-upload-start="uploading = true"
+                    x-on:livewire-upload-finish="uploading = false"
+                    x-on:livewire-upload-cancel="uploading = false"
+                    x-on:livewire-upload-error="uploading = false"
+                >
                     @php($imageUrl = $form->cover?->temporaryUrl() ?? $book?->getCoverUrl())
                     @if($imageUrl)
                         <img
                             src="{{ $imageUrl }}"
                             class="object-cover size-full"
+                            x-show="!uploading"
                         />
 
                         <div class="absolute inset-0 bg-white/20"></div>
@@ -27,9 +36,9 @@
                             </flux:button>
                         </div>
                     @else
-                        <flux:icon.camera class="size-10" wire:loading.remove wire:target="upload" />
-                        <flux:icon.loading class="size-10" wire:loading wire:target="upload" />
+                        <flux:icon.camera class="size-10" x-show="!uploading" />
                     @endif
+                    <flux:icon.loading class="size-10" x-show="uploading" />
                     <input
                         x-ref="coverInput"
                         type="file"
