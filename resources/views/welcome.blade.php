2230dc3d7e26d211150a4e184048513d20ec3a79
HiCharly <caillon.charly@gmail.com>
2025-09-24 22:09:08 +0200

    feat: add Hotjar tracking component to welcome and head views

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -11,6 +11,8 @@
     <link rel="preconnect" href="https://fonts.bunny.net">
     <link href="https://fonts.bunny.net/css?family=instrument-sans:400,500,600,700" rel="stylesheet" />
 
+    <x-hotjar />
+
     @vite(['resources/css/app.css', 'resources/js/app.js'])
 </head>
 
